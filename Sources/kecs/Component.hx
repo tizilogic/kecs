6a8f54bb7c2675dd6bc647f4169f9c8e8100f5ba
tcdude <tc@tizilogic.com>
2021-03-08 00:41:27 +0100

    Make Component an empty class
    Otherwise every derived class would need to needlessly add a constructor

--- Sources/kecs/Component.hx
+++ Sources/kecs/Component.hx
@@ -2,8 +2,10 @@ package kecs;
 
 
 /**
- * The `Component` is simply an empty interface to leave the implementation up
- * to the needs of the user. A `Component` usually is a container holding data,
- * which is updated/modified by a `kecs.System`.
+ * The `Component` is simply an empty class with an empty constructor to leave
+ * the implementation up to the needs of the user. A `Component` usually is a
+ * container holding data, which is updated/modified by a `kecs.System`.
  */
-interface Component {}
+class Component {
+    public function new() {}
+}
